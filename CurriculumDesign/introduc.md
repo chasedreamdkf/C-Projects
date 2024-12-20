79f5e396706b50d7f2e646440ca927f94e1ac58d
chasedreamdkf <3607068551@qq.com>
2024-12-20 17:41:23 +0800

    多项式加法、减法

---
+++ CurriculumDesign/introduc.md
@@ -0,0 +1,8 @@
+# 这个是数据结构课设 #
+
+&nbsp;&nbsp;&nbsp;&nbsp;数据结构
+- Poly&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;多项式的每一项
+- Polys&nbsp;&nbsp;:&nbsp;&nbsp;多项式
+
+&nbsp;&nbsp;&nbsp;&nbsp;函数介绍
+-
--- CurriculumDesign/solution.cpp
+++ CurriculumDesign/solution.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 // 多项式的每一项
 typedef struct Poly{
-    float c;                          // 多项式每一项的系数
-    float e;                          // 多项式每一项的指数
-    struct Poly* next;              // 多项式的下一项
+    float c;                            // 多项式每一项的系数
+    float e;                            // 多项式每一项的指数
+    struct Poly* next;                  // 多项式的下一项
 } Poly;
 
 // 多项式
 typedef struct Polys{
-    int length;                     // 多项式长度
-    Poly *head;                     // 多项式头节点
+    int length;                         // 多项式长度
+    Poly *head;                         // 多项式头节点
 } Polys;
-
-// 初始化多项式头节点
-void InitPoly(Poly* &head){
-    head = new Poly;
-    head->next = NULL;
-}
 
 // 初始化多项式
 void InitPolys(Polys &P){
     P.length = 0;
-    InitPoly(P.head);
+    P.head = new Poly;
+    P.head->next = NULL;
 }
 
 // 建立多项式(尾插法)
 void CreatePolys(Polys &P){
     static int count = 1;
     Poly* rear = P.head;
-    cout << "请输入第" << count << "个多项式项数: ";
+    cout << "请输入多项式" << "P" << count << "的项数: ";
     cin >> P.length;
     for(int i = 1; i <= P.length; i++){
         Poly* p = new Poly;
@@ -54,19 +50,19 @@ void OutPutPolys(Polys P){
     }
     for(int i = 0; i < P.length; i++){
         if(i && p->c > 0) cout << "+";
-        if(p->c != 1) cout << p->c;
-        else if(p->c == -1) cout << "-";
+        if(p->c == -1) cout << "-";
+        else if(p->c != 1) cout << p->c;
         if(p->e != 0){
             cout << "x";
             if(p->e != 1) cout << "^" << p->e;
         }
-        // else if(p->e)
+        else if(!p->e) cout << p->c;
         p = p->next;
     }
     cout << endl;
 }
 
-// 按指数e降序对多项式排序
+// 按指数e降序对多项式排序(冒泡)
 void SortPolys(Polys &P){
     Poly* p = P.head->next;
     for(int i = 0; i < P.length; i++){
@@ -91,12 +87,24 @@ void SortPolys(Polys &P){
 Polys AddPolys(Polys P1, Polys P2){
     SortPolys(P1);
     SortPolys(P2);
-    Polys newP;
-    InitPolys(newP);
-    int count1 = 0, count2 = 0;;
+
+    // 计数器，判定是否到达多项式尾部(不知为什么创建多项式时会多处一项，c、e超出范围)
+    int count1 = 0, count2 = 0;
+
     Poly *p1 = P1.head->next;
     Poly *p2 = P2.head->next;
+
+    // if P1 == 0
+    if(!p1) return P2;
+    // if P2 == 0
+    if(!p2) return P1;
+
+    Polys newP;
+    InitPolys(newP);
+
+    // 尾指针
     Poly *newp = newP.head;
+
     while(count1 < P1.length && count2 < P2.length){
         Poly* p = new Poly;
         // 指数相同, 系数相加
@@ -109,15 +117,15 @@ Polys AddPolys(Polys P1, Polys P2){
             count1++;
             count2++;
         }
-        // 谁指数小, 谁在前面入链表
-        else if(p1->e < p2->e){
-            p->c = p1->c;
+        // 谁指数大, 谁在前面入链表
+        else if(p1->e > p2->e){
+            p->c = p1->c + 0;                   // 此时，与p1.e相等的p2.e的系数c=0
             p->e = p1->e;
             count1++;
             p1 = p1->next;
         }
-        else if(p1->e > p2->e){
-            p->c = p2->c;
+        else if(p1->e < p2->e){
+            p->c = p2->c + 0;                   // 此时，与p2.e相等的p1.e的系数c=0
             p->e = p2->e;
             count2++;
             p2 = p2->next;
@@ -141,15 +149,45 @@ Polys AddPolys(Polys P1, Polys P2){
     return newP;
 }
 
+// 多项式减法
 Polys SubPolys(Polys P1, Polys P2){
     SortPolys(P1);
     SortPolys(P2);
-    Polys newP;
-    InitPolys(newP);
-    int count1 = 0, count2 = 0;;
+
+    // 计数器，判定是否到达多项式尾部(不知为什么创建多项式时会多处一项，c、e超出范围)
+    int count1 = 0, count2 = 0;
+
     Poly *p1 = P1.head->next;
     Poly *p2 = P2.head->next;
+
+    Polys newP;
+    InitPolys(newP);
+
+    // 尾指针
     Poly *newp = newP.head;
+
+    // if P1 == 0
+    if(!p1){
+        // P1 = 0时，对P2的系数取相反数
+        while(count2 < P2.length){
+            Poly* p = new Poly;
+            // 取相反数
+            p->c = -p2->c;
+            p->e = p2->e;
+            p->next = NULL;
+
+            newp->next = p;
+            newp = p;
+            newP.length++;
+            
+            count2++;
+            p2 = p2->next;
+        }
+        return newP;
+    }
+    // if P2 == 0
+    if(!p2) return P1;
+
     while(count1 < P1.length && count2 < P2.length){
         Poly* p = new Poly;
         // 指数相同, 系数相减
@@ -162,15 +200,15 @@ Polys SubPolys(Polys P1, Polys P2){
             count1++;
             count2++;
         }
-        // 谁指数小, 谁在前面入链表
-        else if(p1->e < p2->e){
-            p->c = p1->c;
+        // 谁指数大, 谁在前面入链表
+        else if(p1->e > p2->e){
+            p->c = p1->c - 0;                       // 此时，与p1.e相等的p2.e的系数c=0
             p->e = p1->e;
             count1++;
             p1 = p1->next;
         }
-        else if(p1->e > p2->e){
-            p->c = p2->c;
+        else if(p1->e < p2->e){
+            p->c = 0 - p2->c;                      // 此时，与p2.e相等的p1.e的系数c=0
             p->e = p2->e;
             count2++;
             p2 = p2->next;
@@ -189,8 +227,42 @@ Polys SubPolys(Polys P1, Polys P2){
     }
     else if(count2 < P2.length){
         newP.length += P2.length - count2;
-        newp->next = p2;
+        while(count2 < P2.length){
+            Poly* p = new Poly;
+            // 取相反数
+            p->c = -p2->c;
+            p->e = p2->e;
+            p->next = NULL;
+
+            newp->next = p;
+            newp = p;
+            
+            count2++;
+            p2 = p2->next;
+        }
     }
+    return newP;
+}
+
+// 多项式在x处的值
+float Polys_Val_In_X(Polys P){
+    float X, sum = 0;
+    cout << "请输入X的值: ";
+    cin >> X;
+    Poly* p = P.head->next;
+    for(int i = 1; i <= P.length; i++, p = p->next){
+        sum += p->c * pow(X, p->e);
+    }
+    return sum;
+}
+
+// 求多项式P的导数P’
+Polys Polys_Derivative(Polys P){
+    Polys newP;
+    InitPolys(newP);
+
+    // 尾指针
+    Poly* newp = newP.head;
     return newP;
 }
 
